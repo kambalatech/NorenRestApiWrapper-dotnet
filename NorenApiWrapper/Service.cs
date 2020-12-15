ae822e733e232d7ac4d494eceee313c9d1aee5eb
KUMAR ANAND <itskumaranand@gmail.com>
2020-12-15 13:37:21 +0530

    SessionCloseCallback added

--- NorenApiWrapper/Service.cs
+++ NorenApiWrapper/Service.cs
@@ -8,10 +8,13 @@ using Websocket.Client;
 
 namespace NorenRestApiWrapper
 {
+    
     public class RESTClient
     {
         private HttpClient client = new HttpClient();
         private string _endPoint;
+        
+        public OnResponse onSessionClose;
         public string endPoint
         {
             get => _endPoint;
@@ -69,8 +72,19 @@ namespace NorenRestApiWrapper
                           data = await responseTask.Result.Content.ReadAsStringAsync();
 
                           Console.WriteLine("Response data: {0}", data);
-                          
-                          response.OnMessageNotify(responseTask.Result, data);
+
+                          if (data == "{\"stat\":\"Not_Ok\",\"emsg\":\"Session Expired : Invalid Session Key\"}")
+                          {
+                              //Console.WriteLine("call logout");
+                              LogoutResponse logout = new LogoutResponse();
+                              logout.emsg = "Session Expired : Invalid Session Key";
+                              logout.stat = "Not_Ok";
+                              
+                              onSessionClose?.Invoke(logout, false);
+                          }
+                              
+                          else 
+                              response.OnMessageNotify(responseTask.Result, data);
                       }
                   });
             
